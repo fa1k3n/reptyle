3236c620210d7b75b93abcf042df6f765e93f5e0
Johan Falkenström <johan.falkenstrom@gmail.com>
2019-05-17 14:46:04 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Reptyle',
+      version='0.1',
+      description='REPL toolkit for python',
+      author='Johan Falkenström',
+      author_email='johan.falkenstrom@gmail.com',
+      packages=['reptyle'],
+     )
